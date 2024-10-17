c15d6b71442cd928964417ecc3923fc989577a47
rschube <ryan@stomale.cc>
2024-10-17 00:33:34 -0400

    init

---
+++ eslint.config.mjs
@@ -0,0 +1,20 @@
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import prettierPlugin from "eslint-plugin-prettier";
+
+export default [
+  {
+    files: ["src/**/*.ts"],
+    languageOptions: {
+      parser: tsParser,
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin,  // Explicitly define the TypeScript plugin
+      prettier: prettierPlugin,
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,  // Load the recommended TypeScript rules
+      "prettier/prettier": "error",
+    },
+  },
+];
